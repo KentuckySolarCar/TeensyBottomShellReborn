bc36d748ac5eab58b67f1cc5df734325add1de61
nathangalante04 <ngalante2004@gmail.com>
2024-09-28 12:54:56 -0400

    Can & Tests: Added can setup and unit test structure
    Added the can setup files for FlexCAN_T4 and set up basic unit test
    structure.

---
+++ src/common/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <Arduino.h>
+#include "can_setup.h"
+#include <ChRt.h>
+
+
+#endif
---
+++ test/test_embedded/test_main.cpp
@@ -0,0 +1,35 @@
+#include <unity.h>
+#include <Arduino.h>
+
+void setUp(void) {
+    // Optional: Code to run before each test
+}
+
+void tearDown(void) {
+    // Optional: Code to run after each test
+}
+
+/*
+void test_addition(void) {
+    TEST_ASSERT_EQUAL(3, addi.add(1, 2));  // Test if 1 + 2 equals 3
+    TEST_ASSERT_EQUAL(0, addi.add(-1, 1)); // Test if -1 + 1 equals 0
+    TEST_ASSERT_EQUAL(-2, addi.add(-1, -1)); // Test if -1 + -1 equals -2
+}
+*/
+
+void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
+    UNITY_BEGIN();
+    //RUN_TEST(test_addition);
+    UNITY_END();
+}
+
+void loop() {
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(500);
+}
