0408586a5bdbb46c23f03dfbc22e43dafee0bdc2
Ben Carey <benjamin.james.carey3@gmail.com>
2024-09-30 18:39:21 -0400

    Lights: Implemented light router and blinker system.This commit reimplements the lighting system in the bottom shell. It adds all the pin definitions in the teensy 4.1 spec, so that we can compile without errors for now.Because we don't have the CAN id's properly implemented yet, this commit uses a few proxies (the first five defines in light.cpp, specifically) to allow for the code tocompile for now. The system is untested, but it only needs the aforementioned CAN values to be ready to test.# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch light-router-dev# Changes to be committed:#	new file:   .ccls#	new file:   .vscode/settings.json#	new file:   lib/function/function.cpp#	new file:   lib/function/functions.h#	modified:   src/common/common.h#	new file:   src/common/pin_definitions.h#	new file:   src/light/light.cpp#	new file:   src/light/light.h#

---
+++ lib/function/function.cpp
@@ -0,0 +1,6 @@
+#include <functions.h>
+
+
+int addition::add(int a, int b) {
+    return a + b;
+}
---
+++ lib/function/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+class addition {
+
+public:
+    addition() {}
+    int add (int a, int b);
+};
+
+#endif
