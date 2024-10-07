e3ab3b210fdf34cc3b94039e549b9009015ee70c
nathangalante04 <145509540+nathangalante04@users.noreply.github.com>
2024-10-07 18:03:06 -0400

    Revert "Light Router Re-Implementation from old TeensyBottomShell"

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
-#include "common.h"
+#include <Arduino.h>
 
+// put function declarations here:
+int myFunction(int, int);
 
 void setup() {
-
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
 }
 
 void loop() {
-    // Do nothing
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
